33c3190983622a11f7d489a61098ac719ee79662
AdityaBheke <adityabheke34@gmail.com>
2024-06-14 01:11:22 +0530

    Create landing page

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import expressEjsLayouts from 'express-ejs-layouts';
+import path from 'path';
+
+const app = express();
+app.use(expressEjsLayouts);
+app.use(express.static(path.resolve('public')));
+app.set('view engine','ejs');
+app.set('views',path.resolve('src','views'))
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server is listening on port 3000');
+})

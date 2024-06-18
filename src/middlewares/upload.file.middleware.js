e6e003482c97a9e4ba10e8740f159efe5b605ead
AdityaBheke <adityabheke34@gmail.com>
2024-06-18 19:39:26 +0530

    Restore code

--- src/middlewares/upload.file.middleware.js
+++ src/middlewares/upload.file.middleware.js
@@ -4,7 +4,7 @@ import path from 'path';
 const storage = multer.diskStorage({
     destination: (req, file, cb)=>{
         console.log(path.resolve('public','uploads'));
-        cb(null,path.resolve('uploads'));
+        cb(null,path.resolve('public','uploads'));
     },
     filename: (req,file,cb)=>{
         const filename = Date.now()+'-'+file.originalname;
